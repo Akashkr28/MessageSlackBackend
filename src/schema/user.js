17986ea1c1163f5eb0acf0c310d5a233ffca48f9
Akashkr28 <email.akash2010@gmail.com>
2024-11-27 21:15:07 +0530

    Added users schema

---
+++ src/schema/user.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email already exists"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: [true, "Username already exists"],
+        match: [
+            /^[a-zA-Z0-9]+$/,
+            "Username should only contain letters and numbers",
+        ]
+    },
+    avatar: {
+        type: String,
+    }
+}, {
+    timestamps: true});
+
+// will use to set default avatar before saving username
+userSchema.pre('save', function saveUser(next) {
+    const user = this;
+    user.avatar = `https://robohash.org/${user.username}`;
+    next();
+});  
+
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
