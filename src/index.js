89cbf9c097aa54347f500bbf5a01533566b05623
Akashkr28 <email.akash2010@gmail.com>
2024-12-06 19:23:39 +0530

    Added nodemailer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { StatusCodes } from "http-status-codes";
 
 import connectDB from "./config/dbConfig.js";
+// import mailer from "./config/mailConfig.js";
 import { PORT } from "./config/serverConfig.js";
 import apiRouter from "./router/apiRouter.js";
 
@@ -17,7 +18,14 @@ app.get('/ping', (req, res) => {
         message: "pong"});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
     connectDB();
+    // const mailResponse = await mailer.sendMail({
+    //     from: 'email.akash2010@gmail.com',
+    //     to: 'email.akash2010@gmail.com',
+    //     subject: 'Test email',
+    //     text: 'This is a test email'
+    // });
+    // console.log(mailResponse);
 });
--- src/router/v1/channel.js
+++ src/router/v1/channel.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
+
 import { getChannelByIdController } from '../../controllers/channelController.js';
+import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
 
 const router = express.Router();
 
