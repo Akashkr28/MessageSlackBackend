9a5ce61125210fc8c2ebaaf2823400eb0d608581
Akashkr28 <email.akash2010@gmail.com>
2024-12-06 11:41:36 +0530

    simple linit fix

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -2,10 +2,10 @@ import { StatusCodes } from 'http-status-codes';
 import { v4 as uuidv4 } from 'uuid';
 
 import channelRepository from '../repositories/channelRepository.js';
+import userRepository from '../repositories/userRepository.js';
 import workspaceRepository from "../repositories/workspaceRepository.js";
 import ClientError from '../utils/errors/clientError.js';
 import ValidationError from '../utils/errors/validationError.js';
-import userRepository from '../repositories/userRepository.js';
 
 const isUserAdminOfWorkspace = (workspace, userId) => {
   return workspace.members.find(
