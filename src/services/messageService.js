43bef4dc73f718255f230072ffa59f7fbd596393
Akashkr28 <email.akash2010@gmail.com>
2024-12-12 20:27:48 +0530

    Simple Error Fix

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -12,13 +12,13 @@ export const getMessagesService = async (messageParams, page, limit, user)  => {
 
     const isMember = isUserMemberOfWorkspace(workspace, user);
 
-    const (!isMember) {
+    if (!isMember) {
         throw new ClientError({
             explanation: 'User is not a member of the workspace',
             message: 'User is not a member of the workspace',
             statusCode: StatusCodes.UNAUTHORIZED
-        })
-    }
+        });
+    };
 
     const messages = await messageRepository.getPaginatedMessages(messageParams, page, limit);
     return messages;
